c17200af8ff4e88f1f837a89270727076d01e502
tomoki <tomokiokai0715@gmail.com>
2022-07-17 06:44:53 +0900

    first-commit

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+use App\Http\Requests\AuthorRequest;
+
+class AuthorController extends Controller
+{
+    public function index()
+    {
+        $authors = Author::all();
+        return view('index', ['authors' => $authors]);
+    }
+		public function find()
+    {
+        return view('find', ['input' => '']);
+    }
+    public function search(Request $request)
+    {
+        $author = Author::find($request->input);
+        $param = [
+            'author' => $author,
+            'input' => $request->input
+        ];
+        return view('find', $param);
+    }
+    // 追記：ここから
+    public function add()
+    {
+        return view('add');
+    }
+    public function create(AuthorRequest $request)
+    {
+        $form = $request->all();
+        Author::create($form);
+        return redirect('/');
+    }
+		// 追記：ここまで
+}
